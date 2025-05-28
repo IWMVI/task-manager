76ec2efad138781a6f2ee66e788505d13de6aa40
Wallace Martins <wmvwallace@gmail.com>
2025-05-28 16:02:11 -0300

    feat: Refatora componente de item de tarefa com template, estilos e eventos de conclusão/exclusão

--- src/app/tasks/components/task-item/task-item.component.ts
+++ src/app/tasks/components/task-item/task-item.component.ts
@@ -1,21 +1,26 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Task } from '../../models/task.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-task-item',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.css']
+  styleUrls: ['./task-item.component.scss'],
 })
 export class TaskItemComponent {
   @Input() task!: Task;
-  @Output() delete = new EventEmitter<number>();
-  @Output() complete = new EventEmitter<Task>();
 
-  onDelete() {
-    this.delete.emit(this.task.id);
+  @Output() completeTask = new EventEmitter<Task>();
+  @Output() deleteTask = new EventEmitter<number>();
+
+  complete() {
+    this.completeTask.emit(this.task);
   }
 
-  onComplete() {
-    this.complete.emit(this.task);
+  delete() {
+    this.deleteTask.emit(this.task.id!);
   }
 }
