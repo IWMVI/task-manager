8948f280103ba5da40264b9582f9783b0522ee65
Wallace Martins <wmvwallace@gmail.com>
2025-05-28 16:03:28 -0300

    feat: Atualiza módulo de tarefas para incluir componentes e módulos necessários

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    TaskFormComponent,
+    TaskListComponent,
+    TaskItemComponent,
+  ],
+  exports: [
+    TaskFormComponent,
+    TaskListComponent,
+    TaskItemComponent,
+  ],
 })
-export class TasksModule { }
+export class TasksModule {}
