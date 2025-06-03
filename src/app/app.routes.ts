67c0575b5d89b5f75fcd24f847d0a58c9d9410d2
Wallace Martins <wmvwallace@gmail.com>
2025-06-03 12:30:20 -0300

    fix(routes): corrige o caminho do componente Home na configuração de rotas
    - Ajusta o import e o path do componente Home na configuração de rotas principais.
    - Garante que a navegação para a página inicial funcione corretamente ao acessar '/'.
    - Melhora a experiência do usuário ao acessar a aplicação, redirecionando corretamente para o componente Home.
    - Refatora possíveis imports relativos para manter a padronização e facilitar a manutenção futura.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,9 @@ export const routes: Routes = [
   {
     path: 'home',
     loadComponent: () =>
-      import('./home/home.component').then((m) => m.HomeComponent),
+      import('./tasks/components/home/home.component').then(
+        (m) => m.HomeComponent
+      ),
   },
   {
     path: 'tasks',
